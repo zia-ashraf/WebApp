4409f4a558fbb94c00baafa1f77165bd6d7c3c0a
Zia <zeazzzz4@gmail.com>
2020-12-15 08:05:01 +0530

    Removed hardcoded links-'About' and 'Home' buttons.
    -removed the hard coded links for the 'About' and 'Home' buttons in the
    navigation bar
    -hence we needed to add a third parameter i.e. the name in the path given in
    the 'urls.py' file the 'polls' folder

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('about/', views.about),
+    path('about/', views.about, name='about-page'),
     # the names that we give as a third parameter is used for "href" from templates.
-    path('about2/', views.about2)
+    path('about2/', views.about2, name='about2-page')
 ]
