ee75f694754ba29661af3cea927d2b46d2796a79
Zia <zeazzzz4@gmail.com>
2020-12-26 07:55:30 +0530

    ultimately the individual blog can be loaded via route. the problem was with the previous detail.html file hence deleted it!
    -still we need to load the individual blogs from the home page
    IT GIVES A BUG NOW

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
-
+from .views import PostListView, PostDetailView
 
 app_name = 'polls'
 urlpatterns = [
     # path('', views.IndexView.as_view(), name='index'),
-    path('', views.about, name='about-page'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', PostListView.as_view(), name='about-page'),
+    path('post/<pk>/', PostDetailView.as_view(), name='post_detail'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
     # the names that we give as a third parameter is used for "href" from templates.
     path('about2/', views.about2, name='about2-page')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Post
+from django.views.generic import ListView, DetailView
+
 
 from .models import Choice, Question
 # Create your views here.
@@ -12,42 +14,42 @@ from .models import Choice, Question
 #     return HttpResponse("Hello, world.You're at the polls")
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return Question.objects.order_by('-pub_date')[:5]
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
 
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
+
+
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'polls/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 post = [  # DUMMY data in list of dictionary
@@ -79,3 +81,14 @@ def about(request):
 
 def about2(request):
     return render(request, 'polls/about2.html', {'title': 'About'})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'polls/about.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
