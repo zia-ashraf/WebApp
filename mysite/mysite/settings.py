802d49d4f3b282535398d54565328a0054846610
Zia <zeazzzz4@gmail.com>
2021-01-03 12:05:14 +0530

    Environment variables are now setup and ready for usage!!

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('USER_MAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('USER_MAIL_PASSWORD')
