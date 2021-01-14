d050ceb4893cff4ff818fd4027dea173f65ff9ff
Zia <zeazzzz4@gmail.com>
2021-01-14 21:03:49 +0530

    Added a 'STATIC_ROOT'.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # ensuring that the full path to that directory is created correctly no matter what operation system
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
