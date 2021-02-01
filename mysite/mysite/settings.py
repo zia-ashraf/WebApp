8fad46898e915a46661f7ec0dc9bcaf911967551
Zia <zeazzzz4@gmail.com>
2021-02-01 13:57:00 +0530

    deleted a static part

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # ensuring that the full path to that directory is created correctly no matter what operation system
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
